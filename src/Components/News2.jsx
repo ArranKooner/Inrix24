bb172e18e882ebf03a05d971bb1ee94d8082599c
ArranKooner <125759795+ArranKooner@users.noreply.github.com>
2024-11-17 06:00:52 -0800

    Add files via upload

---
+++ src/Components/News2.jsx
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react';
+import debounce from 'lodash.debounce';
+import { useUser } from './UserContext';
+
+const News2 = () => {
+  const { username } = useUser();
+  const [data8, setData8] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = debounce(async () => {
+      try {
+        const res = await fetch(`http://127.0.0.1:5000/?company=microsoft`);
+        if (!res.ok) {
+          throw new Error(`HTTP error! Status: ${res.status}`);
+        }
+        const result = await res.json();
+        setData8(result.outputs[6]);
+      } catch (err) {
+        setError(err.message);
+      }
+    }, 300);
+
+    fetchData();
+    return () => fetchData.cancel();
+  }, [username]);
+
+  return (
+    <div>
+      {data8 ? <p style={{fontSize: '16px'}}>{data8}</p> : <p>Loading...</p>}
+      {error && <p>Error: {error}</p>}
+    </div>
+  );
+};
+
+export default News2;
