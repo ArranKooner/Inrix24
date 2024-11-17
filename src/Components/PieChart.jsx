a2294a88998ee2ee0d0d5bd3aa526cc26407651c
ArranKooner <125759795+ArranKooner@users.noreply.github.com>
2024-11-17 06:01:37 -0800

    Update PieChart.jsx

--- src/Components/PieChart.jsx
+++ src/Components/PieChart.jsx
@@ -1,17 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import { Pie } from 'react-chartjs-2';
 import { Chart as ChartJS, Title, Tooltip, Legend, ArcElement, CategoryScale, LinearScale } from 'chart.js';
+import debounce from "lodash.debounce";
 
 // Register chart.js components
 ChartJS.register(Title, Tooltip, Legend, ArcElement, CategoryScale, LinearScale);
 
 const PieChart = () => {
   // Data for the pie chart
-    const data = {
-        labels: ['You', 'Comp1', 'Comp2'],
+
+    const [data1, setData1] = useState(null); // Fetch the data from the Flask server
+    const [data2, setData2] = useState(null);
+
+    const fetchData = debounce(async () => {
+        try {
+            const res = await fetch(`http://127.0.0.1:5000/?company=microsoft`);
+            if (!res.ok) {
+                throw new Error(`HTTP error! Status: ${res.status}`);
+            }
+            const result = await res.json();
+            setData1(result.outputs[0]);
+            setData2(result.outputs[1]);
+            //console.log("data is : ", result);
+        } catch (err) {
+            setError(err.message);
+            console.error("Fetch error:", err.message);
+        }
+    }, 300);
+
+    useEffect(() => {
+        fetchData();
+
+    return () => fetchData.cancel(); // Cleanup on unmount
+    }, []);
+
+    const states = data1 ? String(data1).split(',') : [];
+    const stateNum = data2 ? String(data2).split(',') : [];
+
+
+    const chartdata = {
+        labels: states,
         datasets: [
             {
-                data: [12, 19, 8],
+                data: stateNum,
                 backgroundColor: ['#ADD8E6', '#B0E0E6', '#8FD9FB'],
                 hoverOffset: 4,
             },
@@ -47,7 +78,7 @@ const PieChart = () => {
     };
     return (
         <div style={{ position: 'relative', width: '100%', height: '90%' }}>
-            <Pie data={data} options={options} />
+            <Pie data={chartdata} options={options} />
         </div>
     );
 }
